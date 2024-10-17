ef79c743386b6fb7e5f0409d2a54f3b5b9a6da4c
Jasper Yang <jyan457@uwo.ca>
2024-10-17 10:39:56 -0400

    display weather not done yet

--- index.js
+++ index.js
@@ -17,4 +17,26 @@ function getWeather(){
 
 function displayWeather(data){
     const tempDivInfo = document.getElementById('temp-div');
+    const weatherInfoDiv = document.getElementById('weather-info');
+    const weatherIcon = document.getElementById('weather-icon');
+    const hourlyForecastDiv = document.getElementById('hourly-forecast');
+
+    weatherInfoDiv.innerHTML = '';
+    hourlyForecastDiv.innerHTML = '';
+    tempDivInfo.innerHTML = '';
+
+    if (data.cod === '404'){
+        weatherInfoDiv.innerHTML = <p>${data.message}</p>;
+    }
+    else{
+        const cityName = data.name;
+        const temperature = Math.round(data.main.temp - 273.15);
+        const description = data.weather[0].description;
+        const iconCode = data.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+
+        const temperatureHTML = `<p>${temperature}°C</p>`;
+        const weatherHtml = `<p>${cityName}</p><p>${description}</p>`;
+
+    }
 }
